5a10436c3866f70e308b804145ee592776064530
DharshanSR <dharshanravindran8@gmail.com>
2024-12-10 22:45:19 +0530

    create the javadoc folder for the javadoc to see in the web

--- Backend/routes/customerRoutes.js
+++ Backend/routes/customerRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createCustomer, loginCustomer, getCustomers, getCustomerById, updateCustomer, deleteCustomer } = require("../controllers/Customer");
+const { createCustomer, loginCustomer } = require("../controllers/Customer");
 const { authenticateToken } = require("../middleware/authenticateToken");
 
 const router = express.Router();
@@ -9,17 +9,5 @@ router.post("/signup", createCustomer);
 
 // Route for customer login
 router.post("/signin", loginCustomer);
-
-// Route to get all customers (GET request)
-router.get("/", authenticateToken, getCustomers);
-
-// Route to get a specific customer by ID (GET request)
-router.get("/customers/:id", authenticateToken, getCustomerById);
-
-// Route to update a customer (PUT request)
-router.put("/customers/:id", authenticateToken, updateCustomer);
-
-// Route to delete a customer (DELETE request)
-router.delete("/customers/:id", authenticateToken, deleteCustomer);
 
 module.exports = router;
